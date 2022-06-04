c38b2952c99183f37150c7d8540a0edb33ef7e2f
Felipe Kinoshita <kinofhek@gmail.com>
2022-06-04 00:38:51 -0300

    Add icon and logo

--- src/main.cpp
+++ src/main.cpp
@@ -43,7 +43,7 @@ Q_DECL_EXPORT int main(int argc, char *argv[])
     aboutData.addAuthor(i18nc("@info:credit", "Felipe Kinoshita"), i18nc("@info:credit", "Author"), QStringLiteral("kinofhek@gmail.com"), QStringLiteral("https://fhek.gitlab.io"));
     aboutData.setBugAddress("https://invent.kde.org/fhek/vail/-/issues/new");
     KAboutData::setApplicationData(aboutData);
-//     QGuiApplication::setWindowIcon(QIcon::fromTheme(QStringLiteral("org.kde.vail")));
+    QGuiApplication::setWindowIcon(QIcon::fromTheme(QStringLiteral("org.kde.vail")));
 
     QQmlApplicationEngine engine;
 
