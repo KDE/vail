3d25525ae2a7b20d4b579aab0d9e796f76946463
Carl Schwan <carl@carlschwan.eu>
2023-01-27 23:19:30 +0100

    Port to use QT_NO_CAST_FROM_ASCII
    Signed-off-by: Carl Schwan <carl@carlschwan.eu>

--- src/controller.cpp
+++ src/controller.cpp
@@ -20,7 +20,7 @@ Controller::~Controller()
 
 void Controller::restoreWindowGeometry(QQuickWindow *window)
 {
-    KConfig dataResource("data", KConfig::SimpleConfig, QStandardPaths::AppDataLocation);
+    KConfig dataResource(QStringLiteral("data"), KConfig::SimpleConfig, QStandardPaths::AppDataLocation);
     KConfigGroup windowGroup(&dataResource, "Window");
     KWindowConfig::restoreWindowSize(window, windowGroup);
     KWindowConfig::restoreWindowPosition(window, windowGroup);
@@ -28,7 +28,7 @@ void Controller::restoreWindowGeometry(QQuickWindow *window)
 
 void Controller::saveWindowGeometry(QQuickWindow *window)
 {
-    KConfig dataResource("data", KConfig::SimpleConfig, QStandardPaths::AppDataLocation);
+    KConfig dataResource(QStringLiteral("data"), KConfig::SimpleConfig, QStandardPaths::AppDataLocation);
     KConfigGroup windowGroup(&dataResource, "Window");
     KWindowConfig::saveWindowPosition(window, windowGroup);
     KWindowConfig::saveWindowSize(window, windowGroup);
@@ -37,7 +37,7 @@ void Controller::saveWindowGeometry(QQuickWindow *window)
 
 void Controller::translate(QString message)
 {
-    QStringList words = message.split(" ");
+    QStringList words = message.split(QStringLiteral(" "));
     QString translatedMessage;
 
     for (auto word : words) {
@@ -45,16 +45,16 @@ void Controller::translate(QString message)
             translatedMessage.append(getMorseFromCharacter(character));
 
             if (word.indexOf(character) == word.length()) {
-                translatedMessage.append("");
+                translatedMessage.append(QString());
             } else {
-                translatedMessage.append("   ");
+                translatedMessage.append(QStringLiteral("   "));
             }
         }
 
         if (words.indexOf(word) == words.length() - 1) {
-            translatedMessage.append("");
+            translatedMessage.append(QString());
         } else {
-            translatedMessage.append(" /    ");
+            translatedMessage.append(QStringLiteral(" /    "));
         }
     }
 
@@ -67,114 +67,114 @@ QString Controller::getMorseFromCharacter(QString character)
 {
     QString morse;
 
-    if (character == 'a' || character == 'A') {
-        morse = QString(". -");
-    } else if (character == 'b' || character == 'B') {
-        morse = QString("- . . .");
-    } else if (character == 'c' || character == 'C') {
-        morse = QString("- . - .");
-    } else if (character == 'd' || character == 'D') {
-        morse = QString("- . .");
-    } else if (character == 'e' || character == 'E') {
-        morse = QString(".");
-    } else if (character == 'f' || character == 'F') {
-        morse = QString(". . - .");
-    } else if (character == 'g' || character == 'G') {
-        morse = QString("- - .");
-    } else if (character == 'h' || character == 'H') {
-        morse = QString(". . . .");
-    } else if (character == 'i' || character == 'I') {
-        morse = QString(". .");
-    } else if (character == 'j' || character == 'J') {
-        morse = QString(". - - -");
-    } else if (character == 'k' || character == 'K') {
-        morse = QString("- . -");
-    } else if (character == 'l' || character == 'L') {
-        morse = QString(". - . .");
-    } else if (character == 'm' || character == 'M') {
-        morse = QString("- -");
-    } else if (character == 'n' || character == 'N') {
-        morse = QString("- .");
-    } else if (character == 'o' || character == 'O') {
-        morse = QString("- - -");
-    } else if (character == 'p' || character == 'P') {
-        morse = QString(". - - .");
-    } else if (character == 'q' || character == 'Q') {
-        morse = QString("- - . -");
-    } else if (character == 'r' || character == 'R') {
-        morse = QString(". - .");
-    } else if (character == 's' || character == 'S') {
-        morse = QString(". . .");
-    } else if (character == 't' || character == 'T') {
-        morse = QString("-");
-    } else if (character == 'u' || character == 'U') {
-        morse = QString(". . -");
-    } else if (character == 'v' || character == 'V') {
-        morse = QString(". . . -");
-    } else if (character == 'w' || character == 'W') {
-        morse = QString(". - -");
-    } else if (character == 'x' || character == 'X') {
-        morse = QString("- . . -");
-    } else if (character == 'y' || character == 'Y') {
-        morse = QString("- . - -");
-    } else if (character == 'z' || character == 'Z') {
-        morse = QString("- - . .");
-    } else if (character == '0') {
-        morse = QString("- - - - -");
-    } else if (character == '1') {
-        morse = QString(". - - - -");
-    } else if (character == '2') {
-        morse = QString(". . - - -");
-    } else if (character == '3') {
-        morse = QString(". . . - -");
-    } else if (character == '4') {
-        morse = QString(". . . . -");
-    } else if (character == '5') {
-        morse = QString(". . . . .");
-    } else if (character == '6') {
-        morse = QString("- . . . .");
-    } else if (character == '7') {
-        morse = QString("- - . . .");
-    } else if (character == '8') {
-        morse = QString("- - - . .");
-    } else if (character == '9') {
-        morse = QString("- - - - .");
-    } else if (character == '!') {
-        morse = QString("- . - . - -");
-    } else if (character == '"') {
-        morse = QString(". - . . - .");
-    } else if (character == "'") {
-        morse = QString(". - - - - .");
-    } else if (character == '$') {
-        morse = QString(". . . - . . -");
-    } else if (character == '&') {
-        morse = QString(". - . . .");
-    } else if (character == '(') {
-        morse = QString("- . - - .");
-    } else if (character == ')') {
-        morse = QString("- . - - . -");
-    } else if (character == '+') {
-        morse = QString(". - . - .");
-    } else if (character == ',') {
-        morse = QString("- - . . - -");
-    } else if (character == '-') {
-        morse = QString("- . . . . -");
-    } else if (character == '.') {
-        morse = QString(". - . - . -");
-    } else if (character == '/') {
-        morse = QString("- . . - .");
-    } else if (character == ':') {
-        morse = QString("- - - . . .");
-    } else if (character == ';') {
-        morse = QString("- . - . - .");
-    } else if (character == '=') {
-        morse = QString("- . . . -");
-    } else if (character == '?') {
-        morse = QString(". . - - . .");
-    } else if (character == '@') {
-        morse = QString(". - - . - .");
-    } else if (character == '_') {
-        morse = QString(". . - - . -");
+    if (character == QLatin1Char('a') || character == QLatin1Char('A')) {
+        morse = QStringLiteral(". -");
+    } else if (character == QLatin1Char('b') || character == QLatin1Char('B')) {
+        morse = QStringLiteral("- . . .");
+    } else if (character == QLatin1Char('c') || character == QLatin1Char('C')) {
+        morse = QStringLiteral("- . - .");
+    } else if (character == QLatin1Char('d') || character == QLatin1Char('D')) {
+        morse = QStringLiteral("- . .");
+    } else if (character == QLatin1Char('e') || character == QLatin1Char('E')) {
+        morse = QStringLiteral(".");
+    } else if (character == QLatin1Char('f') || character == QLatin1Char('F')) {
+        morse = QStringLiteral(". . - .");
+    } else if (character == QLatin1Char('g') || character == QLatin1Char('G')) {
+        morse = QStringLiteral("- - .");
+    } else if (character == QLatin1Char('h') || character == QLatin1Char('H')) {
+        morse = QStringLiteral(". . . .");
+    } else if (character == QLatin1Char('i') || character == QLatin1Char('I')) {
+        morse = QStringLiteral(". .");
+    } else if (character == QLatin1Char('j') || character == QLatin1Char('J')) {
+        morse = QStringLiteral(". - - -");
+    } else if (character == QLatin1Char('k') || character == QLatin1Char('K')) {
+        morse = QStringLiteral("- . -");
+    } else if (character == QLatin1Char('l') || character == QLatin1Char('L')) {
+        morse = QStringLiteral(". - . .");
+    } else if (character == QLatin1Char('m') || character == QLatin1Char('M')) {
+        morse = QStringLiteral("- -");
+    } else if (character == QLatin1Char('n') || character == QLatin1Char('N')) {
+        morse = QStringLiteral("- .");
+    } else if (character == QLatin1Char('o') || character == QLatin1Char('O')) {
+        morse = QStringLiteral("- - -");
+    } else if (character == QLatin1Char('p') || character == QLatin1Char('P')) {
+        morse = QStringLiteral(". - - .");
+    } else if (character == QLatin1Char('q') || character == QLatin1Char('Q')) {
+        morse = QStringLiteral("- - . -");
+    } else if (character == QLatin1Char('r') || character == QLatin1Char('R')) {
+        morse = QStringLiteral(". - .");
+    } else if (character == QLatin1Char('s') || character == QLatin1Char('S')) {
+        morse = QStringLiteral(". . .");
+    } else if (character == QLatin1Char('t') || character == QLatin1Char('T')) {
+        morse = QStringLiteral("-");
+    } else if (character == QLatin1Char('u') || character == QLatin1Char('U')) {
+        morse = QStringLiteral(". . -");
+    } else if (character == QLatin1Char('v') || character == QLatin1Char('V')) {
+        morse = QStringLiteral(". . . -");
+    } else if (character == QLatin1Char('w') || character == QLatin1Char('W')) {
+        morse = QStringLiteral(". - -");
+    } else if (character == QLatin1Char('x') || character == QLatin1Char('X')) {
+        morse = QStringLiteral("- . . -");
+    } else if (character == QLatin1Char('y') || character == QLatin1Char('Y')) {
+        morse = QStringLiteral("- . - -");
+    } else if (character == QLatin1Char('z') || character == QLatin1Char('Z')) {
+        morse = QStringLiteral("- - . .");
+    } else if (character == QLatin1Char('0')) {
+        morse = QStringLiteral("- - - - -");
+    } else if (character == QLatin1Char('1')) {
+        morse = QStringLiteral(". - - - -");
+    } else if (character == QLatin1Char('2')) {
+        morse = QStringLiteral(". . - - -");
+    } else if (character == QLatin1Char('3')) {
+        morse = QStringLiteral(". . . - -");
+    } else if (character == QLatin1Char('4')) {
+        morse = QStringLiteral(". . . . -");
+    } else if (character == QLatin1Char('5')) {
+        morse = QStringLiteral(". . . . .");
+    } else if (character == QLatin1Char('6')) {
+        morse = QStringLiteral("- . . . .");
+    } else if (character == QLatin1Char('7')) {
+        morse = QStringLiteral("- - . . .");
+    } else if (character == QLatin1Char('8')) {
+        morse = QStringLiteral("- - - . .");
+    } else if (character == QLatin1Char('9')) {
+        morse = QStringLiteral("- - - - .");
+    } else if (character == QLatin1Char('!')) {
+        morse = QStringLiteral("- . - . - -");
+    } else if (character == QLatin1Char('"')) {
+        morse = QStringLiteral(". - . . - .");
+    } else if (character == QLatin1Char(39)) { // '
+        morse = QStringLiteral(". - - - - .");
+    } else if (character == QLatin1Char('$')) {
+        morse = QStringLiteral(". . . - . . -");
+    } else if (character == QLatin1Char('&')) {
+        morse = QStringLiteral(". - . . .");
+    } else if (character == QLatin1Char('(')) {
+        morse = QStringLiteral("- . - - .");
+    } else if (character == QLatin1Char(')')) {
+        morse = QStringLiteral("- . - - . -");
+    } else if (character == QLatin1Char('+')) {
+        morse = QStringLiteral(". - . - .");
+    } else if (character == QLatin1Char(',')) {
+        morse = QStringLiteral("- - . . - -");
+    } else if (character == QLatin1Char('-')) {
+        morse = QStringLiteral("- . . . . -");
+    } else if (character == QLatin1Char('.')) {
+        morse = QStringLiteral(". - . - . -");
+    } else if (character == QLatin1Char('/')) {
+        morse = QStringLiteral("- . . - .");
+    } else if (character == QLatin1Char(':')) {
+        morse = QStringLiteral("- - - . . .");
+    } else if (character == QLatin1Char(';')) {
+        morse = QStringLiteral("- . - . - .");
+    } else if (character == QLatin1Char('=')) {
+        morse = QStringLiteral("- . . . -");
+    } else if (character == QLatin1Char('?')) {
+        morse = QStringLiteral(". . - - . .");
+    } else if (character == QLatin1Char('@')) {
+        morse = QStringLiteral(". - - . - .");
+    } else if (character == QLatin1Char('_')) {
+        morse = QStringLiteral(". . - - . -");
     }
 
     return morse;
